3a7eb3e300b6f05dc7a0784ef9302b1ca1236e48
apooravm <medal.apoorav@gmail.com>
2026-01-10 19:23:03 +0000

    added scaling. *sigh*

--- main.c
+++ main.c
@@ -11,6 +11,8 @@
 #define HEIGHT 480
 #define COLOUR_WHITE 0xffffffff
 
+SDL_Surface* SURFACE;
+
 struct Circle {
 	double x;
 	double y;
@@ -22,7 +24,19 @@ typedef struct {
 	double x, y, z, w;
 } Vec4 ;
 
-void FillCircle(SDL_Surface* surface, struct Circle circle, Uint32 colour) {
+typedef struct {
+	Vec4 vecs[3];
+} Triangle;
+
+typedef struct {
+	Triangle *tris;
+	size_t numTris;
+} Mesh;
+
+Mesh* SquareMesh;
+Mesh* CubeMesh;
+
+void FillCircle(struct Circle circle, Uint32 colour) {
 	double radius_squared = circle.r * circle.r;
 	for (double y = circle.y - circle.r; y <= circle.y + circle.r; y++) {
 		for (double x = circle.x - circle.r; x <= circle.x + circle.r; x++) {
@@ -32,18 +46,63 @@ void FillCircle(SDL_Surface* surface, struct Circle circle, Uint32 colour) {
 
 			if (distance_squared < radius_squared) {
 				SDL_Rect pixel = (SDL_Rect){x, y, 1, 1};
-				SDL_FillRect(surface, &pixel, colour);
+				SDL_FillRect(SURFACE, &pixel, colour);
 			}
 		}
 	}
 }
 
-void draw_dot(const Vec4* v, SDL_Surface* surface) {
-	int side = 5;
-	double screenX = (v->x + 1) * 0.5 * WIDTH;
-	double screenY = (1 - v->y) * 0.5 * HEIGHT;
-	SDL_Rect pixel = (SDL_Rect){screenX, screenY, side, side};
-	SDL_FillRect(surface, &pixel, COLOUR_WHITE);
+// convert normalized cartesian point to screen point
+void draw_dot(double x, double y) {
+	int side = 2;
+	// double screenX = (x + 1) * 0.5 * WIDTH;
+	// double screenY = (1 - y) * 0.5 * HEIGHT;
+	SDL_Rect pixel = (SDL_Rect){x, y, side, side};
+	SDL_FillRect(SURFACE, &pixel, COLOUR_WHITE);
+}
+
+static inline int norm_to_screen_x(double x) {
+    return (int)((x * 0.5 + 0.5) * (WIDTH - 1));
+}
+
+static inline int norm_to_screen_y(double y) {
+    return (int)(((-y) * 0.5 + 0.5) * (HEIGHT - 1));
+}
+
+void DrawLine(double x1, double y1, double x2, double y2) {
+    // Convert normalized coords to screen pixels
+    int x1i = norm_to_screen_x(x1);
+    int y1i = norm_to_screen_y(y1);
+    int x2i = norm_to_screen_x(x2);
+    int y2i = norm_to_screen_y(y2);
+
+    // Bresenham (unchanged from here)
+    int dx = abs(x2i - x1i);
+    int dy = abs(y2i - y1i);
+
+    int sx = (x1i < x2i) ? 1 : -1;
+    int sy = (y1i < y2i) ? 1 : -1;
+
+    int err = dx - dy;
+
+    while (1) {
+        if (x1i >= 0 && x1i < WIDTH && y1i >= 0 && y1i < HEIGHT)
+            draw_dot(x1i, y1i);
+
+        if (x1i == x2i && y1i == y2i)
+            break;
+
+        int e2 = err * 2;
+
+        if (e2 > -dy) {
+            err -= dy;
+            x1i += sx;
+        }
+        if (e2 < dx) {
+            err += dx;
+            y1i += sy;
+        }
+    }
 }
 
 int main() {
@@ -55,6 +114,7 @@ int main() {
 	}
 
 	SDL_Surface* surface = SDL_GetWindowSurface(window);
+	SURFACE = surface;
 
 	struct Circle circle = {200, 0, 80};
 
@@ -77,7 +137,10 @@ int main() {
 
 		} 
 
-		draw_dot(&v1, surface);
+		// draw_dot(200, 200);
+		DrawLine(-0.5, 0.5, 0.5, -0.5);
+		DrawLine(-1, 0, 1, 0);
+		DrawLine(0, 1, 0, -1);
 
 		// FillCircle(surface, circle, COLOUR_WHITE);
 		SDL_UpdateWindowSurface(window);
